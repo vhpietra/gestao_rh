9b5a0dc13a55a53f4b08b3ee27d43b085dd96b3a
Victor Hugo Tagliapietra <vhpietra@gmail.com>
2022-03-03 11:04:08 -0300

    Crindo app departamentos

--- apps/departamentos/models.py
+++ apps/departamentos/models.py
@@ -1,11 +1,9 @@
 from django.db import models
 
-
-class Empresa(models.Model):
+class Departamento(models.Model):
     nome = models.CharField(
-        max_length=100,
-        help_text='Nome da Empresa'
+        max_length=70
     )
-
+    
     def __str__(self):
         return self.nome
